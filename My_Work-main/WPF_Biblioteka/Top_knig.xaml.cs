7cc338be8b03d031b0415aa4de4db0c9ce727739
Mayweather50 <62692570+Mayweather50@users.noreply.github.com>
2022-06-20 09:53:45 +0300

    666

---
+++ My_Work-main/WPF_Biblioteka/Top_knig.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Biblioteka
+{
+    /// <summary>
+    /// Логика взаимодействия для Top_knig.xaml
+    /// </summary>
+    public partial class Top_knig : Page
+    {
+        public Top_knig()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Sapiens(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
